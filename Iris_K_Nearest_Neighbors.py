9f8e688e71bec1b1b88fcf623120fb1ececf8d62
Jostin <jostinhof@gmail.com>
2019-03-14 10:10:49 +0100

    K-NN added

---
+++ Iris_K_Nearest_Neighbors.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 09:56:18 2019
+
+@author: josti
+"""
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import pandas as pd
+names = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'class']
+dataset = pd.read_csv('iris.csv',names=names)
+
+X=dataset.iloc[:,:-1].values
+y=dataset.iloc[:,4].values
+
+from sklearn.preprocessing import LabelEncoder
+labelencoder= LabelEncoder()
+
+y=labelencoder.fit_transform(y)
+
+
+from sklearn.cross_validation import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=1/3, random_state=42)
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+classifier= KNeighborsClassifier(n_neighbors=5,metric='minkowski',p=2)
+
+classifier.fit(X_train,y_train)
+
+y_pred=classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+
+cm=confusion_matrix(y_test,y_pred)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Which-Iris-Am-I
+Which-Iris-Am-I analyses a data set for Iris flowers using different machine learning techniques and predicts the type of flower depends on the 
+charactristics of the flower.
